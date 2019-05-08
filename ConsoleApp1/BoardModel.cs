8e1b6a81b80264a990388e60d1fd97715ddb75d9
Jonathan Robinson <yrobinson@gmail.com>
2019-05-08 11:06:01 -0400

    Add project files.

---
+++ ConsoleApp1/BoardModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Timers;
+
+namespace ConsoleApp1
+{
+    public class BoardModel
+    {
+        private Random randomGen = new Random();
+        public BoardModel()
+        {
+            SetTimer();
+        }
+        public enum State
+        {
+            Blank,
+            Bomb
+        }
+        public delegate void Foo(Point p, State state);
+
+        public event Foo BoardChanges;
+
+        #region TimerDetails
+
+        private System.Timers.Timer _aTimer;
+        private void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            _aTimer = new System.Timers.Timer(2000);
+            // Hook up the Elapsed event for the timer. 
+            _aTimer.Elapsed += OnTimedEvent;
+            _aTimer.AutoReset = true;
+            _aTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Point p = new Point(randomGen.Next(), randomGen.Next());
+            BoardChanges?.Invoke(p, State.Bomb);
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+            _aTimer.Stop();
+            _aTimer.Dispose();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BoardModel boardModel = new BoardModel();
+            boardModel.BoardChanges += CallBackSubscriber;
+
+            Console.WriteLine("\nPress the Enter key to exit the application...\n");
+            Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+            Console.ReadLine();
+            boardModel.Dispose();
+
+
+            Console.WriteLine("Terminating the application...");
+
+        }
+
+
+        private static void CallBackSubscriber(Point p, BoardModel.State state)
+        {
+            Console.WriteLine(p + state.ToString());
+        }
+    }
+}
