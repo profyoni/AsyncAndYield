8ff9b7bec7c45c26381c867b2af64f934cff3284
J Robinson <jrobinson@touro.edu>
2019-05-20 19:52:30 -0400

    f

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,25 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
+       
     {
+        
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult Game()
         {
-            return View();
+            MinesweeperModel msModel = new MinesweeperModel(3, 7);
+            Session["msmodel"] = msModel;
+            return View(msModel);
         }
         public ActionResult GameMove(int x, int y)
         {
-           // dynamic model;
-           // model.MakeMove(x, y);
-
-            return View("Game");
+            var msModel = (MinesweeperModel)Session["msmodel"];
+            msModel.MakeMove(x, y);
+            return View("Game", msModel);
+        }
+        public ActionResult Flag(int x, int y)
+        {
+            var msModel = (MinesweeperModel)Session["msmodel"];
+            msModel.MakeMove(x, y);
+            return View("Game", msModel);
         }
 
         public ActionResult About()
---
+++ WebApplication1/Models/MinesweeperModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class MinesweeperModel
+    {
+        private readonly bool[,]  _board;
+        public int SizeX { get; }
+        public int SizeY { get; }
+
+        public MinesweeperModel(int x, int y)
+        {
+            SizeX = x;
+            SizeY = y;
+            _board = new bool[SizeX,SizeY];
+        }
+
+        public void MakeMove(int x, int y)
+        {
+            _board[x, y] = !_board[x, y];
+        }
+        public bool this[int x, int y]
+        {
+            get { return _board[x, y]; }
+        }
+
+    }
+}
